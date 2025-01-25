60b46d02d3e2b984e9c25c6545b1227bba787e4c
developer <developer@mediatek.com>
2025-01-25 12:02:29 +0800

    [Prpl][feed-mediatek][Add autobuild folder]
    [Description]
    Add autobuild folder
    
    [Release-log]
    N/A
    
    Change-Id: Ifea8b7eb85c5e8c650ad0bed1e9d2df241377902

---
+++ autobuild/autobuild.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Copyright (C) 2024 MediaTek Inc. All rights reserved.
+# Author: Evelyn Tsai <evelyn.tsai@mediatek.com>
+
+# Function to apply patches
+mtk_prplos_patch() {
+    echo -e "\033[1;32m === Applying patches... ===\033[0m"
+
+    # Set the patches directory
+    patches_dir="${prplos_root}/autobuild/prplos/patches"
+
+    # Apply each .patch file in the patches directory
+    for patch in "${patches_dir}"/*.patch; do
+        if [ -f "$patch" ]; then
+            echo -e "\033[1;34m Applying patch: ${patch}... \033[0m"
+            # Apply the patch
+            patch -p1 < "$patch"
+            if [ $? -ne 0 ]; then
+                echo -e "\033[1;31m Failed to apply patch: ${patch} \033[0m"
+                exit 1
+            fi
+        else
+            echo -e "\033[1;33m No patches found in ${patches_dir} \033[0m"
+        fi
+    done
+}
+
+
+# Set prplos_root variable to the current directory
+prplos_root="$(pwd)"
+echo -e "\033[1;32m === Setting prplos_root to the current directory: ${prplos_root} === \033[0m"
+mtk_prplos_patch
+
+# Construct the prplos_cmd command
+prplos_cmd="${prplos_root}/scripts/gen_config.py"
+
+# Add up to four parameters to the prplos_cmd command
+for param in "$@"; do
+    prplos_cmd+=" $param"
+done
+echo -e "\033[1;32m === prplos_cmd command: ${prplos_cmd} === \033[0m"
+
+# Execute the prplos_cmd command
+echo -e "\033[1;32m === Executing prplos_cmd command...=== \033[0m"
+$prplos_cmd
+
+# Create symbolic link
+if test -d "${prplos_root}/../dl"; then
+	if ! test -d "${prplos_root}/dl" -o -L "${prplos_root}/dl"; then
+		 echo -e "\033[1;32m === Executing symbolic link command... ===\033[0m"
+		 ln -sf ../dl ${prplos_root}/dl
+	fi
+fi
+
+# Execute the make command using 32 threads
+echo -e "\033[1;32m === Executing make command... ===\033[0m"
+make -j32
